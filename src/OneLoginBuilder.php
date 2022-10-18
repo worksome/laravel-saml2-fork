f64849f6e03b03bea75b786d8476c099a1b20212
Owen Voke <development@voke.dev>
2022-10-18 09:45:46 +0100

    chore: use `bind` instead of `scoped`

--- src/OneLoginBuilder.php
+++ src/OneLoginBuilder.php
@@ -65,7 +65,7 @@ class OneLoginBuilder
             OneLoginUtils::setProxyVars(true);
         }
 
-        $this->app->scoped('OneLogin_Saml2_Auth', function ($app) {
+        $this->app->bind('OneLogin_Saml2_Auth', function ($app) {
             $config = $app['config']['saml2'];
 
             $this->setConfigDefaultValues($config);
@@ -83,7 +83,7 @@ class OneLoginBuilder
             return new OneLoginAuth($oneLoginConfig);
         });
 
-        $this->app->scoped('Slides\Saml2\Auth', function ($app) {
+        $this->app->bind('Slides\Saml2\Auth', function ($app) {
             return new \Slides\Saml2\Auth($app['OneLogin_Saml2_Auth'], $this->tenant);
         });
     }
